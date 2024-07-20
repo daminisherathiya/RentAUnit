a52542da0b84ba301ef2bf4ddad36849e888e1df
daminisherathiya <daminisherathiya@gmail.com>
2024-07-20 22:00:13 +0530

    The rest of the sections

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,7 +14,7 @@ module.exports = {
       // xl: ["1.25rem", { lineHeight: "1.75rem" }], // 20px/28px
       "2xl": ["1.5rem", { lineHeight: "2rem" }], // 24px/32px
       "3xl": ["2rem", { lineHeight: "3rem" }], // 32px/48px
-      "5xl": ["3.25rem", { lineHeight: "3.875" }], // 52px/62px
+      "5xl": ["3.25rem", { lineHeight: "3.875rem" }], // 52px/62px
       "6xl": ["3.75rem", { lineHeight: "4.5rem" }], // 60px/72px
       "9xl": ["7.5rem", { lineHeight: "11.25rem" }], // 120px/180px
     },
@@ -25,8 +25,10 @@ module.exports = {
         dark: "#0A1039",
         light: "#3C4C5A",
       },
+      light: "#D0E3EF", //Todo
       disabled: "#9EB2BE",
       secondary: "#D80043",
+      medium: "#E4F0F8", //Todo
     },
     backgroundColor: {
       white: "#FFFFFF",
@@ -34,29 +36,47 @@ module.exports = {
       medium: "#E4F0F8",
       secondary: "#D80043",
       primary: {
+        DEFAULT: "#00094A",
         dark: "#0A1039",
       },
     },
     ringColor: {
+      white: "#FFFFFF",
       light: "#D0E3EF",
+      primary: {
+        dark: "#0A1039",
+      },
     },
     dropShadow: {
       elevated: "0px 4px 4px rgba(0, 0, 0, 0.25)",
     },
-    // screens: {
-    //   DEFAULT: "100%",
-    //   sm: "640px",
-    //   md: "768px",
-    //   lg: "1024px",
-    //   xl: "1344px",
-    //   "2xl": "1700px",
-    // },
+    screens: {
+      DEFAULT: "100%",
+      sm: "640px",
+      md: "768px",
+      lg: "1020px",
+      xl: "1340px",
+      "2xl": "1696px",
+    },
     extend: {
       borderRadius: {
         "4xl": "2rem",
       },
+      animation: {
+        marquee: "marquee 25s linear infinite",
+        marquee2: "marquee2 25s linear infinite",
+      },
+      keyframes: {
+        marquee: {
+          "0%": { transform: "translateX(0%)" },
+          "100%": { transform: "translateX(-100%)" },
+        },
+        marquee2: {
+          "0%": { transform: "translateX(100%)" },
+          "100%": { transform: "translateX(0%)" },
+        },
+      },
     },
   },
   plugins: [],
-  prefix: "tw-",
 };
